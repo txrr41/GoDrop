737e3f330510c0ff72f5f89b3c955a90f952a9c2
txrr41 <kauantxr41@gmail.com>
2025-12-29 15:46:52 -0300

    ✨ feat: Adiciona estrutura html

---
+++ src/main/java/com/project/shiphub/ShiphubApplication.java
@@ -0,0 +1,13 @@
+package com.project.shiphub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiphubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShiphubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/shiphub/ShiphubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.shiphub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiphubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
