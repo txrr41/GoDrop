c7e3076af71c7d52073fe15d6e3dfe262895481d
txrr41 <kauantxr41@gmail.com>
2026-01-05 15:24:36 -0300

    ✨ feat(class): Adiciona LoginRepository

--- src/main/java/com/project/shiphub/login/repository/LoginRepository.java
+++ src/main/java/com/project/shiphub/login/repository/LoginRepository.java
@@ -1,4 +1,13 @@
 package com.project.shiphub.login.repository;
 
-public class LoginRepository {
+import com.project.shiphub.login.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface LoginRepository extends JpaRepository<User,Long> {
+    Optional<User> findByEmailAndPassword(String email, Long password);
+
+    boolean existsByEmailAndPassword(String email, Long password);
 }
