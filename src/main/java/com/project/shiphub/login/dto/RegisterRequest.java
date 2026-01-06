adbc9625106990f33797ffe3beddcf82d5b0fd84
txrr41 <kauantxr41@gmail.com>
2026-01-06 14:53:00 -0300

    ✨ feat(class): Adiciona Register Request

---
+++ src/main/java/com/project/shiphub/login/dto/RegisterRequest.java
@@ -0,0 +1,13 @@
+package com.project.shiphub.login.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequest {
+    private String email;
+    private String senha;
+}
