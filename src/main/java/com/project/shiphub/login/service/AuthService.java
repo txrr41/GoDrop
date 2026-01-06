e7d490d7f7cdd730e054ed914f6db977492d0536
txrr41 <kauantxr41@gmail.com>
2026-01-06 15:32:18 -0300

    ✨ feat: Adiciona validação para cadastro de usuario

--- src/main/java/com/project/shiphub/login/service/AuthService.java
+++ src/main/java/com/project/shiphub/login/service/AuthService.java
@@ -2,11 +2,15 @@ package com.project.shiphub.login.service;
 
 import com.project.shiphub.login.dto.LoginRequest;
 import com.project.shiphub.login.dto.LoginResponse;
+import com.project.shiphub.login.dto.RegisterRequest;
+import com.project.shiphub.login.dto.RegisterResponse;
 import com.project.shiphub.login.model.User;
 import com.project.shiphub.login.repository.LoginRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Service
@@ -44,6 +48,31 @@ public class AuthService {
         response.setEmail(user.getEmail());
 
         return response;
+    }
+
+    public RegisterResponse cadastrar(RegisterRequest request) throws Exception {
+        if (request.getEmail() == null || request.getEmail().isEmpty()) {
+            throw new Exception("E-mail é obrigatório");
+        }
+
+        if (request.getSenha() == null || request.getSenha().isEmpty()) {
+            throw new Exception("Senha é obrigatório");
+        }
+
+        if (loginRepository.existsByEmail(request.getEmail())) {
+            throw new Exception("E-mail já cadastrado");
+        }
+
+        User user = new User ();
+        user.setEmail(request.getEmail());
+        user.setPassword((passwordEncoder.encode(request.getSenha())));
+        user.setDataCriacao(LocalDateTime.now());
+
+        User savedUser = loginRepository.save(user);
+
+        return new RegisterResponse(
+                "Usuário cadastrado com sucesso!", savedUser.getId(), savedUser.getEmail()
+        );
     }
 
 }
