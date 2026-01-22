b7a48c55b4c33aabc68bd70d03a041553d3ad3ad
txrr41 <kauantxr41@gmail.com>
2026-01-22 15:13:28 -0300

    ✨feat: Configura vuetify

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,20 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createVuetify } from 'vuetify'
+import 'vuetify/styles'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
 import App from './App.vue'
 import router from './router'
 
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
 createApp(App)
+    .use(vuetify)
     .use(createPinia())
     .use(router)
     .mount('#app')
