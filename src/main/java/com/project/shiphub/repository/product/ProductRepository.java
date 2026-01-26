11e3090c0111f484ba52a7ed3dd94963d8ffcd80
txrr41 <kauantxr41@gmail.com>
2026-01-26 17:22:08 -0300

    ✨ feat: Adiciona repository produtos

--- src/main/java/com/project/shiphub/repository/product/ProductRepository.java
+++ src/main/java/com/project/shiphub/repository/product/ProductRepository.java
@@ -1,4 +1,11 @@
 package com.project.shiphub.repository.product;
 
-public class ProductRepository {
+import com.project.shiphub.model.product.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product,Long> {
+
+
 }
